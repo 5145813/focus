53ae3b255ceedcb73315e898894d2fb5e439a852
5145813 <5145813@gmail.com>
2012-12-09 07:53:05 +0800

    看

--- focus/focus.js
+++ focus/focus.js
@@ -6,7 +6,7 @@ $(function() {
 		modalSelector : ".modal",
 		focusClass : "focus",
 		enterKey : 13,
-		precision : 20
+		precision : 10
 	};
 
 	var limit = {
@@ -32,7 +32,7 @@ $(function() {
 	};
 
 	$.fn.getRect = function() {
-		var rect = this.getBoundingClientRect();
+		var rect = this[0].getBoundingClientRect();
 
 		var precisedRect = {
 			left : precise(rect.left),
@@ -46,21 +46,68 @@ $(function() {
 		return new Rectangle(precisedRect);
 	};
 
+	var oldFocus = $.fn.focus;
+
+	$.fn.focus = function() {
+		if (0 == arguments.length) {
+			new Focus().getFocusElement().removeClass(o.focusClass);
+
+			var $this = $(this);
+			$this.addClass(o.focusClass);
+		}
+
+		return oldFocus.apply(this, arguments);
+	};
+
+	$.fn.extend({
+		isFocusable : function() {
+			return $(this).is(o.enableSelector);
+		}
+	});
+
 	Rectangle.prototype = {
-		scan : function(handler, direction) {
-			if (direction.isAscend()) {
-				var count = (limit[direction.getOppositeName()] - this[direction.getName()]) / o.precision;
-				for ( var i = 0; i < count; i++) {
-					if (false == handler(i)) {
+
+		search : function(direction, handler) {
+			var start = this[direction.getName()] / o.precision;
+			var end = limit[direction.getName()] / o.precision;
+
+			if (start < end) {
+				for ( var i = start + 1; i <= end; i++) {
+					console.log(1);
+					if (false == handler(i * o.precision)) {
 						break;
 					}
 				}
 			} else {
-				var count = limit[direction.getOppositeName()] / o.precision;
-				for ( var i = count - 1; i >= 0; i++) {
-					if (false == handler(i)) {
+				for ( var i = start - 1; i >= 0; i--) {
+					console.log(1);
+					if (false == handler(i * o.precision)) {
 						break;
 					}
+				}
+			}
+		},
+
+		inner : function(direction, handler) {
+			var start = Math.min(this[direction.getName()], this[direction.getOppositeName()]);
+			var width = Math.abs(this[direction.getName()] - this[direction.getOppositeName()]);
+			var count = width / o.precision;
+			for ( var i = 1; i <= count; i++) {
+				console.log(1);
+				if (!handler(start + i * o.precision)) {
+					break;
+				}
+			}
+		},
+
+		outer : function(direction, handler) {
+			var start = Math.min(this[direction.getName()], this[direction.getOppositeName()]);
+			var width = Math.abs(this[direction.getName()] - this[direction.getOppositeName()]);
+			var count = width / o.precision;
+			for ( var i = 1; i <= count; i++) {
+				console.log(1);
+				if (!handler(start + i * o.precision)) {
+					break;
 				}
 			}
 		}
@@ -84,19 +131,11 @@ $(function() {
 		},
 
 		getOppositeDirection : function() {
-			return new Direction(37 + (this._keyCode - 37 + 1) % 4);
+			return new Direction(37 + (this._keyCode + 1) % 4);
 		},
 
 		getRotateDirection : function() {
-			return new Direction(37 + (this._keyCode - 37 - 1) % 4);
-		},
-
-		isAscend : function() {
-			return _.include([ "right", "down" ], this.getName());
-		},
-
-		isX : function() {
-			return _
+			return new Direction(this._keyCode + (0 == this._keyCode % 2 ? -1 : 1));
 		}
 	};
 
@@ -119,16 +158,46 @@ $(function() {
 		},
 
 		move : function(direction) {
-			var $focus = getFocusElement().first();
-			var rect = $focus.getRect();
-
+			var $focus = this.getFocusElement().first();
+			var focusRect = $focus.getRect();
 			var rotateDirection = direction.getRotateDirection();
 
-			rect.scan(function(i) {
-				rotateDirection.each(function(j) {
-				}, direction);
+			var nextElementArr = [];
+
+			var isX = _.include([ "left", "right" ], rotateDirection.getName());
+
+			var isFind = false;
+			focusRect.search(direction, function(b) {
+				focusRect.inner(rotateDirection, function(a) {
+					var x, y;
+					if (isX) {
+						x = a;
+						y = b;
+					} else {
+						x = b;
+						y = a;
+					}
+
+					var element = document.elementFromPoint(x, y);
+					if (element && $(element).isFocusable()) {
+						nextElementArr.push(element);
+						isFind = true;
+					}
+
+					if (isFind) {
+						return false;
+					}
+				});
+
+				return !isFind;
 			});
 
+			nextElementArr = _.uniq(nextElementArr);
+
+			if (!_.isEmpty(nextElementArr)) {
+				var $next = $(nextElementArr[0]);
+				$next.focus();
+			}
 		}
 	};
 
@@ -148,7 +217,7 @@ $(function() {
 		start : function(options) {
 			_.extend(o, options);
 			limit.right = precise(screen.width);
-			limit.height = precise(screen.width);
+			limit.down = precise(screen.height);
 		}
 	};
 });
